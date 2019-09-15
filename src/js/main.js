d426b16305a908bc697bc5ccc43d14d8989ca7be
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2019-09-15 06:08:35 +0200

    feat(test): add js scripts

--- src/js/main.js
+++ src/js/main.js
@@ -2,21 +2,38 @@
 
 // service worker registration - remove if you're not going to use it
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker.register("serviceworker.js").then(
+      function(registration) {
+        // Registration was successful
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      },
+      function(err) {
+        // registration failed :(
+        console.log("ServiceWorker registration failed: ", err);
+      }
+    );
   });
 }
 
 // place your code below
 
+const btnBeEco = document.querySelector(".welcome-screen__button--be-eco-js");
+const btnStart = document.querySelector(".welcome-screen__button--start-js");
+const inputName = document.querySelector(".welcome-screen__button--name-js");
+const welcomeParagraph = document.querySelector(
+  ".welcome-screen__paragraph--js"
+);
 
-console.log(`Hello world!`);
+function showContent() {
+  btnStart.removeAttribute("hidden");
+  inputName.removeAttribute("hidden");
+  btnBeEco.setAttribute("hidden", true);
+  welcomeParagraph.setAttribute("hidden", true);
+}
 
-
+btnBeEco.addEventListener("click", showContent);
